4a5368981ea4564a44d906c52e692b79b2664c08
suryadesu <cs17btech11048@iith.ac.in>
2022-06-24 15:49:57 +0530

    fixed run.sh

--- run.sh
+++ run.sh
@@ -8,12 +8,12 @@ mkdir figures
 
 cd prism/
 
-$PATH_TO_PRISM/prism model.prism prop.props -const mu=50,tau=0:5:50,pe=0.5,pm=0.5,tgen=5,tbsm=10 -exportresults ../results/model-tau.csv:csv -ptamethod digital
-$PATH_TO_PRISM/prism model.prism prop.props -const mu=10:10:100,tau=15,pe=0.5,pm=0.5,tgen=5,tbsm=10 -exportresults ../results/model-mu.csv:csv -ptamethod digital
-$PATH_TO_PRISM/prism model.prism prop.props -const mu=50,tau=15,pe=0:0.1:1,pm=0.5,tgen=5,tbsm=10 -exportresults ../results/model-pgen.csv:csv -ptamethod digital
-$PATH_TO_PRISM/prism model.prism prop.props -const mu=50,tau=15,pe=0.5,pm=0:0.1:1,tgen=5,tbsm=10 -exportresults ../results/model-pbsm.csv:csv -ptamethod digital
-$PATH_TO_PRISM/prism model.prism prop.props -const mu=100,tau=50,pe=0.5,pm=0.5,tgen=0:5:50,tbsm=50 -exportresults ../results/model-tgen.csv:csv -ptamethod digital
-$PATH_TO_PRISM/prism model.prism prop.props -const mu=100,tau=50,pe=0.5,pm=0.5,tgen=10,tbsm=0:5:50 -exportresults ../results/model-tbsm.csv:csv -ptamethod digital
+$PATH_TO_PRISM/prism model.prism prop.props -const mu=50,tau=0:5:50,pgen=0.5,pbsm=0.5,tgen=5,tbsm=10 -exportresults ../results/model-tau.csv:csv -ptamethod digital
+$PATH_TO_PRISM/prism model.prism prop.props -const mu=10:10:100,tau=15,pgen=0.5,pbsm=0.5,tgen=5,tbsm=10 -exportresults ../results/model-mu.csv:csv -ptamethod digital
+$PATH_TO_PRISM/prism model.prism prop.props -const mu=50,tau=15,pgen=0:0.1:1,pbsm=0.5,tgen=5,tbsm=10 -exportresults ../results/model-pgen.csv:csv -ptamethod digital
+$PATH_TO_PRISM/prism model.prism prop.props -const mu=50,tau=15,pgen=0.5,pbsm=0:0.1:1,tgen=5,tbsm=10 -exportresults ../results/model-pbsm.csv:csv -ptamethod digital
+$PATH_TO_PRISM/prism model.prism prop.props -const mu=100,tau=50,pgen=0.5,pbsm=0.5,tgen=0:5:50,tbsm=50 -exportresults ../results/model-tgen.csv:csv -ptamethod digital
+$PATH_TO_PRISM/prism model.prism prop.props -const mu=100,tau=50,pgen=0.5,pbsm=0.5,tgen=10,tbsm=0:5:50 -exportresults ../results/model-tbsm.csv:csv -ptamethod digital
 
 cd ../
 
